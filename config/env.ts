dd2e502f5a1e33b3343a0486cf45fa7aa35391e2
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2026-06-25 23:26:03 +0530

    '

--- config/env.ts
+++ config/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.string().url().default('http://localhost:5000/api'),
+  NEXT_PUBLIC_API_BASE_URL: z.string().url().default('https://uniblox-assigment-rudransh.vercel.app/api'),
 });
 
 const _env = envSchema.safeParse({
